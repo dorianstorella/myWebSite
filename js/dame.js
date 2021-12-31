c8469701c4d7c99f67e1d0a0df71d290492f4c8f
Dorian Storella <dorianstorella@gmail.com>
2021-12-31 23:18:57 +0100

    ajout plateau

--- js/dame.js
+++ js/dame.js
@@ -3,5 +3,16 @@ game = document.getElementById("newGame");
 game.addEventListener("click", startGame) 
 
 function startGame() {
-    console.log("hello world")
+    let tray = [
+        ["pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0],
+        [0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR"],
+        ["  ", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ["  ", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ["  ", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW"],   
+        ["pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0],
+    ]
 }
