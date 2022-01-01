d20eebd91386178a83719ccdcfd0b374b56d0a5b
Dorian Storella <dorianstorella@gmail.com>
2022-01-01 16:32:53 +0100

    plateau fini

--- js/dame.js
+++ js/dame.js
@@ -2,17 +2,28 @@ game = document.getElementById("newGame");
 
 game.addEventListener("click", startGame) 
 
-function startGame() {
+function startGame() 
+{
     let tray = [
         ["pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0],
         [0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR"],
-        ["  ", 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ["  ", 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ["  ", 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW"],   
-        ["pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0],
+        ["  ", 0, "  ", 0, "  ", 0, "  ", 0, "  ", 0],
+        [0, "  ", 0, "  ", 0, "  ", 0, "  ", 0, "  "],
+        ["  ", 0, "  ", 0, " ", 0, "  ", 0, "  ", 0],
+        [0, "  ", 0, "  ", 0, "  ", 0, "  ", 0, "  "],
+        ["  ", 0, "  ", 0, "  ", 0, "  ", 0, "  ", 0],
+        [0, "  ", 0, "  ", 0, "  ", 0, "  ", 0, "  "],
+        ["pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW",0],   
+        [0,"pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW"],
     ]
+    // tray.forEach(element => {
+    //     console.log(element+"")
+    // });
+    return tray
+}
+function game () 
+{
+    tray.forEach(element => {
+        console.log(element+"")
+    });
 }
