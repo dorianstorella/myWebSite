c58c85f44b4105bae1d119efbbc1f1e1dfd84afd
dorianstorella <dorianstorella@gmail.com>
2022-01-18 17:33:22 +0100

    ajout plateau de jeux

--- js/dame.js
+++ js/dame.js
@@ -1,48 +1,156 @@
+
 game = document.getElementById("newGame");
 
 game.addEventListener("click", startGame) 
+
+const rougeInput = document.createElement("input")
+rougeInput.setAttribute("type", "radio")
+rougeInput.setAttribute("name","color")
+rougeInput.setAttribute("value","rouge")
+
+const rougeLabel = document.createElement("label")
+rougeLabel.setAttribute("type","rouge")
+rougeLabel.innerHTML = "rouge"
+
+const blancInput = document.createElement("input")
+blancInput.setAttribute("type", "radio")
+blancInput.setAttribute("name","color")
+blancInput.setAttribute("value","blanc")
+
+const blancLabel = document.createElement("label")
+blancLabel.setAttribute("type","blanc")
+blancLabel.innerHTML = "blanc"
+
+document.body.insertBefore(rougeLabel, game)
+document.body.insertBefore(rougeInput,rougeLabel)
+document.body.insertBefore(blancLabel, game)
+document.body.insertBefore(blancInput,blancLabel)
 
 function startGame() 
 {   
 
     game.setAttribute("style","display:none")
-    let tray = [
-        ["pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0],
-        [0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR", 0, "pionR"],
-        ["  ", 0, "  ", 0, "  ", 0, "  ", 0, "  ", 0],
-        [0, "  ", 0, "  ", 0, "  ", 0, "  ", 0, "  "],
-        ["  ", 0, "  ", 0, " ", 0, "  ", 0, "  ", 0],
-        [0, "  ", 0, "  ", 0, "  ", 0, "  ", 0, "  "],
-        ["  ", 0, "  ", 0, "  ", 0, "  ", 0, "  ", 0],
-        [0, "  ", 0, "  ", 0, "  ", 0, "  ", 0, "  "],
-        ["pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW",0],   
-        [0,"pionW", 0, "pionW", 0, "pionW", 0, "pionW", 0, "pionW"],
+    //refaire avec un for
+    let tray = 
+    [
+        ["pionR", 000, "pionR", 000, "pionR", 000, "pionR",000, "pionR", 000],
+        [000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR"],
+        ["pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000],
+        [000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR"],
+        ["  ", 000, "  ", 000, "  ", 000, "  ", 000, "  ", 000],
+        [000, "  ", 000, "  ", 000, "  ", 000, "  ", 000, "  "],
+        ["pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000],
+        [000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW"],
+        ["pionW", 000, "pionW", 000, "pionW", 0, "pionW", 000, "pionW",000],   
+        [000,"pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW"],
     ]
+   // console.log(tray[0][0])
+    color = document.querySelector("input[name=color]:checked").value;
+    
+   
+
+    rougeInput.setAttribute("style","display:none")
+    rougeLabel.setAttribute("style","display:none")
+    blancInput.setAttribute("style","display:none")
+    blancLabel.setAttribute("style","display:none")
+    
+
+    plateau = document.createElement("table")
+    plateau.setAttribute("style", "background-color:black;height:500px;width:500px;")
+    document.getElementById('body').appendChild(plateau)
+   
+    tr = document.createElement("tr")
+    plateau.appendChild(tr)
+    
     // tray.forEach(element => {
     //     console.log(element+"")
     // });
-   
-    const rougeInput = document.createElement("input")
-    rougeInput.setAttribute("type", "radio")
-    rougeInput.setAttribute("name","color")
-    rougeInput.setAttribute("value","rouge")
+  
     
-    const rougeLabel = document.createElement("label")
-    rougeLabel.setAttribute("type","rouge")
-    rougeLabel.innerHTML = "rouge"
-    
-    const blancInput = document.createElement("input")
-    blancInput.setAttribute("type", "radio")
-    blancInput.setAttribute("name","color")
-    blancInput.setAttribute("value","blanc")
-    
-    const blancLabel = document.createElement("label")
-    blancLabel.setAttribute("type","blanc")
-    blancLabel.innerHTML = "blanc"
+    for (let y = 0 ; y < 10 ; y++)
+    {   
+      
+        for (let x = 0 ; x < 10 ; x++ )
+        {   
+            if (y % 2 == 0)
+            {
+                if (x % 2 == 0 )
+                {
+                    div = document.createElement("th")
+                    div.setAttribute("style"," color:red;background-color:brown;height:50px;width:50px; ")
+                    tr.appendChild(div)
+                }
+                else if (x % 2 != 0)
+                {
+                    div = document.createElement("th")
+                    div.setAttribute("style"," color:red;background-color:#fff;height:50px;width:50px;")
+                    tr.appendChild(div)
+                }
+            }
 
-    document.body.insertBefore(rougeLabel, game)
-    document.body.insertBefore(rougeInput,rougeLabel)
-    document.body.insertBefore(blancLabel, game)
-    document.body.insertBefore(blancInput,blancLabel)
+            else if (y % 2 != 0)
+            {
+                if (x % 2 == 0 )
+                {
+                    div = document.createElement("th")
+                    div.setAttribute("style"," color:red;background-color:white;height:50px;width:50px; ")
+                    tr.appendChild(div)
+                }
+                else if (x % 2 != 0)
+                {
+                    div = document.createElement("th")
+                    div.setAttribute("style"," color:red;background-color:brown;height:20px;width:20px; ")
+                    tr.appendChild(div)
+                }
+            }   
+            /*if (tray[y][x] === "pionR")
+            {
+                div = document.createElement("th")
+                div.setAttribute("style"," color:red;background-color:brown;height:20px;width:20px; ")
+                div.innerHTML = "rouge"
+                tr.appendChild(div)
+            }
+            
+            if (tray[y][x] === "pionW")
+            {
+                div = document.createElement("th")
+                div.setAttribute("style","color:white;background-color:brown;height:20px;width:20px; ")
+                div.innerHTML = "blanc"
+                tr.appendChild(div)
+            }
+            if (tray[y][x] === 000 )
+            {
+                div = document.createElement("th")
+                div.setAttribute("style","background-color:white;height:20px;width:20px; ")
+                tr.appendChild(div)
+
+            }
+            else if (tray[y][x] === "  ")
+            {
+                div = document.createElement("th")
+                div.setAttribute("style","background-color:white;height:20px;width:20px; ")
+                tr.appendChild(div)
+            }*/
+            
+        }
+
+        if (y < 9)
+        
+            tr = document.createElement("tr")
+            plateau.appendChild(tr)
+    
+    }
+}
+
+function deplacement()
+{
+    console.log(trayval);
+    console.log(tray );
+    // deplacement : pour pouvoir se deplacer remarque
+    // selon le pion de couleur il doit aller plus haut ou plus bas 
+    // et doit etre inferieure ou supperieure de 1 mais strictement 1
+    // le rouge  avance de bas en haut tanids que les blacn se deplace de haut en bas
+    // si il a un pion de sa couleure le pion  ne peux avancer sur cette case la 
+
 }
 
