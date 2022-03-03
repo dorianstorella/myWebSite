7d81420a19c6a86149e9f87e363d793d504b0681
dorianstorella <dorianstorella@gmail.com>
2022-03-03 22:35:21 +0100

    ajout tour par tour  commentaire pour la suite <recuparation pion saute >

--- js/dame.js
+++ js/dame.js
@@ -27,20 +27,19 @@ document.body.insertBefore(rougeInput,rougeLabel)
 document.body.insertBefore(blancLabel, game)
 document.body.insertBefore(blancInput,blancLabel)
 
+// blanc commence
 tourcolor = "rouge"
-tourcolor2 = "blanc"
+count = 0
 let tray = 
 [
-    ["pionR", 000, "pionR", 000, "pionR", 000, "pionR",000, "pionR", 000],
-    [000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR"],
-    ["pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000],
-    [000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR"],
-    ["  ", 000, "  ", 000, "  ", 000, "  ", 000, "  ", 000],
-    [000, "  ", 000, "  ", 000, "  ", 000, "  ", 000, "  "],
-    ["pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000],
-    [000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW"],
-    ["pionW", 000, "pionW", 000, "pionW", 0, "pionW", 000, "pionW",000],   
-    [000,"pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW"],
+    ["pionR", 000, "pionR", 000, "pionR", 000, "pionR",000],
+    [000, "pionR", 000, "pionR", 000, "pionR", 000, "pionR"],
+    ["pionR", 000, "pionR", 000, "pionR", 000, "pionR", 000],
+    ["  ", 000, "  ", 000, "  ", 000, "  ", 000],
+    [000, "  ", 000, "  ", 000, "  ", 000, "  "],
+    [000, "pionW", 000, "pionW", 000, "pionW", 000, "pionW"],
+    ["pionW", 000, "pionW", 000, "pionW", 000, "pionW", 000],   
+    [000,"pionW", 000, "pionW", 000, "pionW", 000, "pionW"]
 ]
 function startGame() 
 {   
@@ -56,7 +55,7 @@ function startGame()
    
     affichage()
     
-    forgame()
+
    
         //afficher regle
     
@@ -70,7 +69,7 @@ function updateGame()
 }
 
 function selectionPion(pion)
-{   
+{    
     parentligne = pion.explicitOriginalTarget.parentElement.parentElement.previousElementSibling
     parentnext = pion.explicitOriginalTarget.parentElement.parentElement.nextElementSibling
     cellx = pion.explicitOriginalTarget.offsetParent.cellIndex
@@ -86,12 +85,32 @@ function selectionPion(pion)
     if (tourcolor == "rouge")
     {
         
-        for (let i = 0; i < 10; i++)
+        for (let i = 1; i < 9; i++)
         {
-            if (tray[cellY+1][cellx+1] == "  "|| tray[cellY+1][cellx-1] == "  ")
+            if (tray[cellY+1][cellx+1] == "  ")
             {
                 //console.log(thisCellRed[i])
+                let possible = thisCellRed[cellx+1]
+                
+                //mettre a toute les case possible cet event
+                possible.addEventListener("click", selectionCase)
+                
+            }
+            else if(tray[cellY+1][cellx+1] == "pionW" && tray[cellY+2][cellx+2] == "  " )
+            {
+               console.log("hello")
+            }
+            else 
+            {
                 let possible = thisCellRed[i]
+                possible.removeEventListener("click", selectionCase)
+                
+            }
+
+            if (tray[cellY+1][cellx-1] == "  ")
+            {
+                //console.log(thisCellRed[i])
+                let possible = thisCellRed[cellx-1]
                 
                 //mettre a toute les case possible cet event
                 possible.addEventListener("click", selectionCase)
@@ -99,7 +118,7 @@ function selectionPion(pion)
             } 
             else 
             {
-                let possible = thisCellRed[i]
+                let possible = thisCellRed[cellx]
                 possible.removeEventListener("click", selectionCase)
                 
             }
@@ -107,20 +126,38 @@ function selectionPion(pion)
 
     }
 
-    if (tourcolor2 == "blanc")
+    if (tourcolor == "blanc")
     {
-        for (let i = 0; i < 10; i++) 
-        {
-            if (tray[cellY-1][cellx+1] == "  "|| tray[cellY-1][cellx-1] == "  ")
+        for (let i = 0; i < 8; i++) 
+        {   
+
+            //retire tout les a
+            if (tray[cellY-1][cellx+1] == "  ")
             {
-                let possible = thisCellWhite[i]
-                console.log(thisCellWhite[i])
+                let possible = thisCellWhite[cellx+1]
+                console.log(thisCellWhite[cellx+1])
+                possible.addEventListener("click", selectionCase)
+                
+            }
+            else 
+            {
+                let possible = thisCellWhite[cellx+1]
+                possible.removeEventListener("click", selectionCase)
+                
+            }
+
+            if (tray[cellY-1][cellx-1] == "  ")
+            {
+                //console.log(thisCellRed[i])
+                let possible = thisCellWhite[cellx-1]
+                
+                //mettre a toute les case possible cet event
                 possible.addEventListener("click", selectionCase)
                 
             } 
             else 
             {
-                let possible = thisCellWhite[i]
+                let possible = thisCellWhite[cellx-1]
                 possible.removeEventListener("click", selectionCase)
                 
             }
@@ -131,16 +168,17 @@ function selectionPion(pion)
 
 function selectionCase(thiscase)  // a mettre peut etre dans  boucle sur forgame()
 {   
-    cellcaseX = thiscase.originalTarget.cellIndex
-    cellcaseY = thiscase.originalTarget.parentElement.rowIndex
+    cellcaseX = thiscase.originalTarget.cellIndex  
+    cellcaseY = thiscase.originalTarget.parentElement.rowIndex 
     
     temp = tray[cellY][cellx]
     tray[cellY][cellx]  = tray[cellcaseY][cellcaseX]
     tray[cellcaseY][cellcaseX] = temp
     
+   
     console.log(tray[cellcaseY][cellcaseY])
-    
-   affichage() // a remettre en fin de boucle sur forgame()
+    count++
+    affichage() // a remettre en fin de boucle sur forgame()
 
 }
 //faire un fonction pour voir quel pion peut faire un deplacement
@@ -156,11 +194,11 @@ function possibleMovement(tourcolor) // regarder quel boutton peut se deplacer s
             possY = poss[index].offsetParent.parentElement.rowIndex
             possX = poss[index].offsetParent.cellIndex
             
-            if(tray[possY+1][possX+1] == "  " || tray[possY+1][possX-1] == "  " )
+           if(tray[possY+1][possX+1] == "  " || tray[possY+1][possX-1] == "  " )
             {
                 element.addEventListener("click", selectionPion)
             }
-            else if (tray[possY+1][possX+1] == "pionW" && tray[possY+2][possX+2] == "  " )
+            else if (tray[possY+1][possX+1] == "pionW" && tray[possY+2][possX+2] == "  " ) 
             {
                 
                 element.addEventListener("click", selectionPion)
@@ -202,37 +240,10 @@ function possibleMovement(tourcolor) // regarder quel boutton peut se deplacer s
     }
 }
 
-function forgame() // boucle pour simulker la game
-{   
-    game = true
-    
-    nbrpionRouge = 3
-    nbrpionblanc = 3
-    for(i = 0; game; i++)
-    {   
-        if (tourcolor == "rouge")
-        {   
-            // attClick.addEventListener('click', function(){
-            if(nbrpionRouge == 0){game = false}
-            
-            nbrpionRouge--
-            // })
-            tourcolor = "blanc"
-        }
-        if (tourcolor == "blanc")
-        {   
-            if(nbrpionblanc == 0){game = false}
-            
-            nbrpionblanc--
-            
-            tourcolor = "rouge"
-        }
-    }
-}
-
 function affichage()
 {   
-    
+   
+   
     table = document.getElementById('table')
     if (table != null) table.remove()
 
@@ -242,13 +253,13 @@ function affichage()
     plateau.setAttribute("style", "background-color:black;height:500px;width:500px;")
     plateau.setAttribute("id", "table")
     document.getElementById('body').appendChild(plateau)
-    for (let y = 0 ; y < 10 ; y++)
+    for (let y = 0 ; y < 8 ; y++)
     {   
         tr = document.createElement("tr")
-       
+        
         plateau.appendChild(tr)
 
-        for (let x = 0 ; x < 10 ; x++ )
+        for (let x = 0 ; x < 8 ; x++ )
         {   
             if (y % 2 == 0)
             {
@@ -266,7 +277,7 @@ function affichage()
                     tr.appendChild(div)
                 }
             }
-
+                            
             else if (y % 2 != 0)
             {
                 if (x % 2 == 0 )
@@ -303,7 +314,17 @@ function affichage()
                 div.appendChild(piondiv)
             }
            
-            possibleMovement(tourcolor2)
+            //possibleMovement(tourcolor)
+            
+            if (count % 2 == 0)
+            {
+                tourcolor = "rouge"
+            }
+            else
+            {
+                tourcolor = "blanc"
+            }
+            
             possibleMovement(tourcolor)
             
         }
